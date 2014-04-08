56a0a06eb1454ada57f398fa135fa482aa683fdb
Nicholas Serra <nicks@ydekproductions.com>
2014-04-07 23:04:45 -0400

    Very basic test setup.

---
+++ redis_status/tests/test_admin.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+
+class RedisStatusSanityTests(TestCase):
+    urls = 'redis_status.tests.test_urls'
+
+    def setUp(self):
+        self.user = User.objects.create_superuser('test', 'test@test.com', 'password')
+        self.client.login(username=self.user.username, password='password')
+
+    def test_admin_accessible(self):
+        response = self.client.get('/admin/')
+        self.assertEqual(200, response.status_code)
+
+    def test_cache_stats_included(self):
+        response = self.client.get('/admin/')
+        self.assertIn('class="cache_stats"', response.content)
