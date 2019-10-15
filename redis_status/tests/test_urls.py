44d53400dedcec0d989903286c4b833257315216
Nicholas Serra <nickserra@gmail.com>
2019-10-15 00:50:27 -0400

    Django compat

--- redis_status/tests/test_urls.py
+++ redis_status/tests/test_urls.py
@@ -1,11 +1,11 @@
 try:
-    from django.conf.urls import include, url
+    from django.conf.urls import url
 except ImportError:
-    from django.conf.urls.defaults import include, url
+    from django.conf.urls.defaults import url
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
